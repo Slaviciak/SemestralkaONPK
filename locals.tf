9344d6d35a0512b17b17aa885e99fe01fbc9d696
Slavomír Tatarka <slavic112@gmail.com>
2023-12-21 15:03:12 +0100

    rename

--- locals.tf
+++ locals.tf
@@ -1,13 +1,11 @@
-# --- compute/locals.tf ---
-
 locals {
-  kis_os_auth_url = "http://158.193.152.44:5000/v3/"
   kis_os_region = "RegionOne"
-  domain_name = "admin_domain"
+  auth_url      = "https://158.193.152.44:5000/v3/"
 
   kis_os_endpoint_overrides = {
-
-  network = "https://158.193.152.44:9696/v2.0/"
+    network = "https://158.193.152.44:9696/v2.0/"
+    compute = "https://158.193.152.44:8774/v2.1/"
+    image   = "https://158.193.152.44:9292/v2.0/"
   }
+  domain_name = "admin_domain"
 }
-  
